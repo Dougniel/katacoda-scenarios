6a81693baa304fa2a7258dca30bfca5490db00e9
Daniel Santos <dougniel@gmail.com>
2022-08-26 16:23:12 +0000

    feat(kcat): Coloration syntaxique marche pas

--- kcat/init.sh
+++ kcat/init.sh
@@ -1,4 +1,4 @@
 export LANG=en_US.UTF-8
 
 # Terminal plus lisible grâce à la coloration syntaxique
-sudo apt install fish -y && fish
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
