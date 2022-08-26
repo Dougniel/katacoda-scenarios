c09eb2cc3a2a62d407a318ed008b821d1874e2ad
Daniel Santos <dougniel@gmail.com>
2022-08-26 15:50:29 +0000

    feat(kcat): Terminal plus lisible grâce à la coloration syntaxique

--- kcat/init.sh
+++ kcat/init.sh
@@ -1,6 +1,8 @@
 export LANG=en_US.UTF-8
 
 # Terminal plus lisible grâce à la coloration syntaxique
-apt install -y zsh && chsh -s $(which zsh) && zsh
-git clone https://github.com/z-shell/F-Sy-H ~.F-Sy-H
-source ~.F-Sy-H/F-Sy-H.plugin.zsh
+apt install -y zsh 
+chsh -s $(which zsh)
+git clone https://github.com/z-shell/F-Sy-H ~/.F-Sy-H
+echo "~/.F-Sy-H/F-Sy-H.plugin.zsh" >> .zshrc
+zsh
